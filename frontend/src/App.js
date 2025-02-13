22157eb10c032cd0185a6060b349df2ef09a57ac
vbaroni <ext.sopra.vbaroni@grandlyon.com>
2025-02-13 16:14:47 +0100

    fix matomo reference

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import { MainContextProvider } from './contexts/mainContext';
-import React, { useEffect } from 'react';
 
 function App() {
-	
-    // Install/Activate Matomo Tag Manager
-    useEffect(() => {
-        var _mtm = (window._mtm = window._mtm || []);
-        _mtm.push({ 'mtm.startTime': new Date().getTime(), event: 'mtm.Start' });
-        var d = document,
-            g = d.createElement('script'),
-            s = d.getElementsByTagName('script')[0];
-        g.async = true;
-        g.src = 'https://statweb.grandlyon.com/js/container_tiGIcnW4.js';
-        s.parentNode.insertBefore(g, s);
-    }, []);
-
     return (
         <MainContextProvider>
             <div className="App">
